80349a71fa147e6d3cbcb962e5d794b60e52edae
Joseaneb <joseane_bortoli@hotmail.com>
2019-03-17 17:58:56 -0300

    Testando interação entre as classes com as frases do Buzz

--- src/main/java/poo/BuzzLightyear.java
+++ src/main/java/poo/BuzzLightyear.java
@@ -1,4 +1,18 @@
 package poo;
 
+import java.util.Scanner;
+
 public class BuzzLightyear {
+    public void FrasesBuzz (String frases) {
+
+
+        //6 Frases do Buzz
+
+        System.out.println("Ao infinito... e além!");
+        System.out.println("Só quero que saiba que embora tenha tentado me eliminar, vingança não é uma ideia que cultivamos em meu planeta.");
+        System.out.println("Nunca duvidei de mim mesmo, Comandante, e não vou começar agora!");
+        System.out.println("Isto não é voar. Isto é cair, com estilo!");
+        System.out.println("Alvo se aproximando.");
+        System.out.println("Pare!!! Ordeno que pare!");
+    }
 }
--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -1,4 +1,9 @@
 package poo;
 
 public class Principal {
+    public static void main(String[] args) {
+
+        BuzzLightyear ex1 = new BuzzLightyear();
+        ex1.FrasesBuzz("");
+    }
 }
