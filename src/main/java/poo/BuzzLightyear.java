65cc3c2ebe5d863760666dea0015a5e62fb1e0f1
Joseaneb <joseane_bortoli@hotmail.com>
2019-03-17 18:58:25 -0300

    Interacao entre classes testando metodos

--- src/main/java/poo/BuzzLightyear.java
+++ src/main/java/poo/BuzzLightyear.java
@@ -3,16 +3,49 @@ package poo;
 import java.util.Scanner;
 
 public class BuzzLightyear {
-    public void FrasesBuzz (String frases) {
 
+    public void FrasesBuzz () {
 
         //6 Frases do Buzz
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Entre com um número de 1 a 6 para ver as frases do Buzz: ");
+        int frases = teclado.nextInt();
 
-        System.out.println("Ao infinito... e além!");
-        System.out.println("Só quero que saiba que embora tenha tentado me eliminar, vingança não é uma ideia que cultivamos em meu planeta.");
-        System.out.println("Nunca duvidei de mim mesmo, Comandante, e não vou começar agora!");
-        System.out.println("Isto não é voar. Isto é cair, com estilo!");
-        System.out.println("Alvo se aproximando.");
-        System.out.println("Pare!!! Ordeno que pare!");
+        switch (frases){
+
+            case 1: System.out.println("Ao infinito... e além!");
+              break;
+            case 2: System.out.println("Só quero que saiba que embora tenha tentado me eliminar, vingança não é uma ideia que cultivamos em meu planeta.");
+              break;
+            case 3: System.out.println("Nunca duvidei de mim mesmo, Comandante, e não vou começar agora!");
+              break;
+            case 4: System.out.println("Isto não é voar. Isto é cair, com estilo!");
+              break;
+            case 5: System.out.println("Alvo se aproximando.");
+              break;
+            case 6: System.out.println("Pare!!! Ordeno que pare!");
+            break;
+            default:
+                System.out.println("Não é um número válido!");
+        }
+
+
+    }
+
+    public void Capacete(){
+
+        //Capacete retrátil
+        Scanner teclado2 = new Scanner(System.in);
+        System.out.print("Capacete retrátil (Digite 1 para aberto e 2 para fechado): ");
+        int  capacete = teclado2.nextInt();
+
+        if(capacete == 1){
+            System.out.println("Capacete aberto!");
+        }else if(capacete == 2){
+            System.out.println("Capacete fechado!");
+        }
+        else{
+            return;
+        }
     }
 }
--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -4,6 +4,10 @@ public class Principal {
     public static void main(String[] args) {
 
         BuzzLightyear ex1 = new BuzzLightyear();
-        ex1.FrasesBuzz("");
+        ex1.FrasesBuzz();
+
+        System.out.println();
+        BuzzLightyear ex2 = new BuzzLightyear();
+        ex2.Capacete();
     }
 }
