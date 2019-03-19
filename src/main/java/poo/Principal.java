32ab0f8b7708732d7f7d47e5b7aeec9b7c2d86ff
Joseaneb <joseane_bortoli@hotmail.com>
2019-03-19 11:47:33 -0300

    Arrumando a Indentação

--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
 
-        //BuzzLightyear ex1 = new BuzzLightyear();
-        //ex1.FrasesBuzz();
+        // BuzzLightyear ex1 = new BuzzLightyear();
+        // ex1.FrasesBuzz();
 
-      //  System.out.println();
-        //BuzzLightyear ex2 = new BuzzLightyear();
-        //ex2.Capacete();
+        // System.out.println();
+        // BuzzLightyear ex2 = new BuzzLightyear();
+        // ex2.Capacete();
 
 
         int buzz1 = 1;
